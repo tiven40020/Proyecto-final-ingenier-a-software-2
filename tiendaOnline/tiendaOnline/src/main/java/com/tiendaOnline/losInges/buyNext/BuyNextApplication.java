b30e665d3a6a9ac5c49c6ff27c754ace91bd7fc8
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2025-10-01 16:08:52 -0500

    Estructura proyecto

---
+++ tiendaOnline/tiendaOnline/src/main/java/com/tiendaOnline/losInges/buyNext/BuyNextApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaOnline.losInges.buyNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyNextApplication.class, args);
+	}
+
+}
